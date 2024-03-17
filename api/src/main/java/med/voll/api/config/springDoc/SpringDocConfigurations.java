8e72b5fdab3aa0f0ef35bdc5d65803d7d1b7939e
sebastiantorreslab <str9371@gmail.com>
2024-03-17 02:27:02 -0500

    file name changed

--- api/src/main/java/med/voll/api/config/springDoc/SpringDocConfigurations.java
+++ api/src/main/java/med/voll/api/config/springDoc/SpringDocConfigurations.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
-public class SpingDocConfigurations {
+public class SpringDocConfigurations {
 
     @Bean
     public OpenAPI customOpenAPI() {
